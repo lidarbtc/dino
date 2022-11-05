27e13ab76bb2a97315097e572633a62624647e53
lidar <97009593+lidarbtc@users.noreply.github.com>
2022-11-05 14:10:41 +0900

    a

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import re
 
 app = Flask(__name__)
 bcrypt = Bcrypt()
-#app.config["SECRET_KEY"] = os.urandom(55)
+# app.config["SECRET_KEY"] = os.urandom(55)
 app.config["SECRET_KEY"] = f"dsfiji38jhjsdfjojidsfh832hdsifjiopasdkmmvcdikjuhed9w329hd"
 app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(minutes=60)
 
@@ -22,6 +22,7 @@ HTML_PATH_ITEMLIST = './list.html'
 HTML_PATH_SCRAP = './collection.html'
 HTML_PATH_MYPAGE = './mypage.html'
 
+
 def gettotalpage(m, n):
     if m < n:
         return 1
@@ -29,6 +30,7 @@ def gettotalpage(m, n):
         return m // n
     else:
         return m // n + 1
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -53,8 +55,10 @@ def login():
             session['userplan'] = userplan
             return render_template(HTML_PATH_HOME, username=session['user'], userplan=session['userplan'])
         else:
-            return render_template(HTML_PATH_INDEX, ErrorTitle="ERROR! ", ErrorMessage="Username/Password does not exist")
-       
+            return render_template(HTML_PATH_INDEX, ErrorTitle="ERROR! ",
+                                   ErrorMessage="Username/Password does not exist")
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -82,22 +86,25 @@ def register():
         if userpw != reuserpw:
             flash("비밀번호가 다릅니다.")
             return render_template(HTML_PATH_INDEX, ErrorTitle="ERROR! ", ErrorMessage="비밀번호가 다릅니다.")
-        
+
         checkusername = db.db_connector(f'''SELECT userid FROM usertbl WHERE userid="{userid}";''')
         if checkusername == "":
             pw_hash = bcrypt.generate_password_hash(userpw).decode('utf-8')
-            db.db_connector(f"INSERT INTO usertbl(userid, userpw, userplan, username, phone_number, business_number) VALUES('{userid}', '{pw_hash}', 'FREE', '{username}', '{phone_number}', '{business_number}');")
+            db.db_connector(
+                f"INSERT INTO usertbl(userid, userpw, userplan, username, phone_number, business_number) VALUES('{userid}', '{pw_hash}', 'FREE', '{username}', '{phone_number}', '{business_number}');")
             flash("가입에 성공하였습니다.")
             return render_template(HTML_PATH_INDEX, ErrorTitle="NOTICE! ", ErrorMessage="Register Success")
         else:
             flash("이미 가입된 이메일입니다.")
             return render_template(HTML_PATH_REGISTER, ErrorTitle="ERROR! ", ErrorMessage="Username already exist")
 
+
 @app.route('/home', methods=['GET'])
 def home():
     if not check.login:
         return render_template(HTML_PATH_INDEX)
     return render_template(HTML_PATH_HOME, username=session['user'], userplan=session['userplan'])
+
 
 @app.route('/mypage', methods=['GET'])
 def mypage():
@@ -108,27 +115,39 @@ def mypage():
     elif request.method == 'POST':
         return render_template(HTML_PATH_MYPAGE, username=session['user'], userplan=session['userplan'])
 
+
 @app.route('/api_setting', methods=['GET', 'POST'])
 def apiset():
     if not check.login:
         return render_template(HTML_PATH_INDEX)
 
-    if request.method =='GET':
+    if request.method == 'GET':
         userid = session['user']
         try:
-            data = db.db_connector(f'''SELECT ssid, sspw, atid, atpw, cpid, cpcode, cpak, cpsk, cpday, elevenapi, rtapi, rtday, wpid, wppw FROM apikey WHERE userid="{userid}"";''').split()
+            data = db.db_connector(
+                f'''SELECT ssid, sspw, atid, atpw, cpid, cpcode, cpak, cpsk, cpday, elevenapi, rtapi, rtday, wpid, wppw FROM apikey WHERE userid="{userid}"";''').split()
         except:
             return render_template(HTML_PATH_APISET, username=session['user'], userplan=session['userplan'])
 
-        ssid = data[0]; sspw = data[1]
-        atid = data[2]; atpw = data[3]
-        cpid = data[4]; cpcode = data[5]; cpak = data[6]; cpsk = data[7]; cpday = data[8]
+        ssid = data[0];
+        sspw = data[1]
+        atid = data[2];
+        atpw = data[3]
+        cpid = data[4];
+        cpcode = data[5];
+        cpak = data[6];
+        cpsk = data[7];
+        cpday = data[8]
         elevenapi = data[9]
-        rtapi = data[10]; rtday = data[11]
-        wpid = data[12]; wppw = data[13]    
-        return render_template(HTML_PATH_APISET, username=session['user'], userplan=session['userplan'], ssid=ssid, sspw=sspw, atid=atid, atpw=atpw, cpid=cpid, cpcode=cpcode, cpak=cpak, cpsk=cpsk, cpday=cpday, elevenapi=elevenapi, rtapi=rtapi, rtday=rtday, wpid=wpid, wppw=wppw)
+        rtapi = data[10];
+        rtday = data[11]
+        wpid = data[12];
+        wppw = data[13]
+        return render_template(HTML_PATH_APISET, username=session['user'], userplan=session['userplan'], ssid=ssid,
+                               sspw=sspw, atid=atid, atpw=atpw, cpid=cpid, cpcode=cpcode, cpak=cpak, cpsk=cpsk,
+                               cpday=cpday, elevenapi=elevenapi, rtapi=rtapi, rtday=rtday, wpid=wpid, wppw=wppw)
 
-    elif request.method =='POST':
+    elif request.method == 'POST':
         ssid = request.form.get('ssid')
         sspw = request.form.get('sspw')
         atid = request.form.get('atid')
@@ -144,9 +163,13 @@ def apiset():
         wpid = request.form.get('wpid')
         wppw = request.form.get('wppw')
 
-        db.db_connector(f"REPLACE INTO apikey(userid, ssid, sspw, atid, atpw, cpid, cpcode, cpak, cpsk, cpday, elevenapi, rtapi, rtday, wpid, wppw) VALUES('{session['user']}', '{ssid}', '{sspw}', '{atid}', '{atpw}', '{cpid}', '{cpcode}', '{cpak}', '{cpsk}', '{cpday}', '{elevenapi}', '{rtapi}', '{rtday}', '{wpid}', '{wppw}');")
+        db.db_connector(
+            f"REPLACE INTO apikey(userid, ssid, sspw, atid, atpw, cpid, cpcode, cpak, cpsk, cpday, elevenapi, rtapi, rtday, wpid, wppw) VALUES('{session['user']}', '{ssid}', '{sspw}', '{atid}', '{atpw}', '{cpid}', '{cpcode}', '{cpak}', '{cpsk}', '{cpday}', '{elevenapi}', '{rtapi}', '{rtday}', '{wpid}', '{wppw}');")
         flash('적용되었습니다.')
-        return render_template(HTML_PATH_APISET, username=session['user'], userplan=session['userplan'], ssid=ssid, sspw=sspw, atid=atid, atpw=atpw, cpid=cpid, cpcode=cpcode, cpak=cpak, cpsk=cpsk, cpday=cpday, elevenapi=elevenapi, rtapi=rtapi, rtday=rtday, wpid=wpid, wppw=wppw)
+        return render_template(HTML_PATH_APISET, username=session['user'], userplan=session['userplan'], ssid=ssid,
+                               sspw=sspw, atid=atid, atpw=atpw, cpid=cpid, cpcode=cpcode, cpak=cpak, cpsk=cpsk,
+                               cpday=cpday, elevenapi=elevenapi, rtapi=rtapi, rtday=rtday, wpid=wpid, wppw=wppw)
+
 
 @app.route('/itemlist/<int:listid>', methods=['GET'])
 def itemlist(listid):
@@ -162,22 +185,23 @@ def itemlist(listid):
         descimg = []
         for i in productid:
             title.append(db.db_connector(f'''SELECT title FROM taobao WHERE productid="{i}";'''))
-            prop.append(db.db_connector(f'''SELECT propid, propname, price, img FROM prop WHERE productid="{i}";''').split())
+            prop.append(
+                db.db_connector(f'''SELECT propid, propname, price, img FROM prop WHERE productid="{i}";''').split())
             taoimg.append(db.db_connector(f'''SELECT img FROM taobaoimg WHERE productid="{i}";''').split())
             descimg.append(db.db_connector(f'''SELECT img FROM descimg WHERE productid="{i}";''').split())
-        
+
     except:
         return render_template(HTML_PATH_ITEMLIST, username=session['user'], userplan=session['userplan'], isitem=False)
 
     pagecount = gettotalpage(len(productid), 20)
 
     # 없는 페이지를 접속 시 아이템을 반환하지 않음.
-    if pagecount-1 < listid:
+    if pagecount - 1 < listid:
         return render_template(HTML_PATH_ITEMLIST, username=session['user'], userplan=session['userplan'], isitem=False)
 
     if listid != 1:
         startpage = (listid * 20) - 20
-        endpage = ((listid+1) * 20) + 1 - 20
+        endpage = ((listid + 1) * 20) + 1 - 20
     else:
         startpage = 0
         endpage = 20
@@ -187,7 +211,7 @@ def itemlist(listid):
     prop2 = []
     taoimg2 = []
     descimg2 = []
-    
+
     try:
         for i in range(startpage, endpage):
             productid2.append(productid[i])
@@ -200,8 +224,10 @@ def itemlist(listid):
         print(startpage, endpage)
 
         endpage = startpage + 1
-        return render_template(HTML_PATH_ITEMLIST, username=session['user'], userplan=session['userplan'], isitem=True, productid=productid2, title=title2, prop=prop2, taoimg=taoimg2, descimg=descimg2, listid=listid, endpage=endpage, startpage=startpage)
-    
+        return render_template(HTML_PATH_ITEMLIST, username=session['user'], userplan=session['userplan'], isitem=True,
+                               productid=productid2, title=title2, prop=prop2, taoimg=taoimg2, descimg=descimg2,
+                               listid=listid, endpage=endpage, startpage=startpage)
+
     startpage = startpage // 20
 
     if startpage == 0:
@@ -209,7 +235,10 @@ def itemlist(listid):
 
     endpage = startpage + 1
 
-    return render_template(HTML_PATH_ITEMLIST, username=session['user'], userplan=session['userplan'], isitem=True, productid=productid2, title=title2, prop=prop2, taoimg=taoimg2, descimg=descimg2, listid=listid, endpage=endpage, startpage=startpage)
+    return render_template(HTML_PATH_ITEMLIST, username=session['user'], userplan=session['userplan'], isitem=True,
+                           productid=productid2, title=title2, prop=prop2, taoimg=taoimg2, descimg=descimg2,
+                           listid=listid, endpage=endpage, startpage=startpage)
+
 
 @app.route('/itemlist', methods=['POST'])
 def itemlistup():
@@ -280,6 +309,7 @@ def itemlistup():
 
     return redirect(url_for('itemlist', listid=listid))
 
+
 @app.route('/itemscrap', methods=['GET', 'POST'])
 def itemscrap():
     if not check.login:
@@ -293,72 +323,88 @@ def itemscrap():
             a = re.findall("\d{12}(?=&)|(?=\s)", taobao)
 
             sample_list = [v for v in a if v]
-            
+
             success_count = 0
             fail_count = 0
             for id in sample_list:
                 try:
                     result = api.taobao(id)
 
-                    product_id = str(result['item']['num_iid']) # 상품번호
-                    title = result['item']['title'] # 상품제목
-                    price = result['item']['price'] # 상품 가격(할인 적용)
-                    orginal_price = result['item']['orginal_price'] # 상품 가격 (할인 미적용)
-                    product_imgurl = result['item']['item_imgs'] # 상품 사진 링크
-                    desc_imgurl = result['item']['desc_img'] # 상품 설명 사진, 없을 수 도 있음.
-                    prop = result['item']['skus']['sku'] # 옵션
-                    prop_imgurl = result['item']['prop_imgs']['prop_img'] # 옵션 사진
-                    
+                    product_id = str(result['item']['num_iid'])  # 상품번호
+                    title = result['item']['title']  # 상품제목
+                    price = result['item']['price']  # 상품 가격(할인 적용)
+                    orginal_price = result['item']['orginal_price']  # 상품 가격 (할인 미적용)
+                    product_imgurl = result['item']['item_imgs']  # 상품 사진 링크
+                    desc_imgurl = result['item']['desc_img']  # 상품 설명 사진, 없을 수 도 있음.
+                    prop = result['item']['skus']['sku']  # 옵션
+                    prop_imgurl = result['item']['prop_imgs']['prop_img']  # 옵션 사진
+
                     for i, n in enumerate(product_imgurl):
-                        threading.Thread(target=get.imgpt, args=(n["url"], product_id, i)).start() # 사진을 폴더에 저장
+                        threading.Thread(target=get.imgpt, args=(n["url"], product_id, i)).start()  # 사진을 폴더에 저장
                         product_img = "{}_{}".format(product_id, i)
-                        product_img = "../static/taobao_img/"+product_img+".png"
-                        db.db_connector(f"INSERT INTO taobaoimg(productid, img) VALUES('{product_id}', '{product_img}');")
+                        product_img = "../static/taobao_img/" + product_img + ".png"
+                        db.db_connector(
+                            f"INSERT INTO taobaoimg(productid, img) VALUES('{product_id}', '{product_img}');")
 
                     if len(desc_imgurl) > 0:
                         for i, n in enumerate(desc_imgurl):
-                            threading.Thread(target=get.imgdc, args=(n, product_id, i, product_id)).start() # 사진을 폴더에 저장
+                            threading.Thread(target=get.imgdc,
+                                             args=(n, product_id, i, product_id)).start()  # 사진을 폴더에 저장
                             desc_img = "{}_{}".format(product_id, i)
-                            desc_img = "../static/desc_img/"+desc_img+".png"
-                            db.db_connector(f"INSERT INTO descimg(productid, img) VALUES('{product_id}', '{desc_img}');")
-                    
+                            desc_img = "../static/desc_img/" + desc_img + ".png"
+                            db.db_connector(
+                                f"INSERT INTO descimg(productid, img) VALUES('{product_id}', '{desc_img}');")
+
                     for i, n in enumerate(prop):
                         prop_name = n['properties_name'].split(':')[3]
                         prop_filename = n['sku_id']
                         try:
-                            threading.Thread(target=get.imgpp, args=(prop_imgurl[0]['url'], prop_filename, i)).start() # 사진을 폴더에 저장
+                            threading.Thread(target=get.imgpp,
+                                             args=(prop_imgurl[0]['url'], prop_filename, i)).start()  # 사진을 폴더에 저장
                         except:
                             # 사진이 없는것
                             try:
-                                threading.Thread(target=get.imgpp, args=(prop_imgurl[0]['url'], prop_filename, i)).start() # 사진을 폴더에 저장
+                                threading.Thread(target=get.imgpp,
+                                                 args=(prop_imgurl[0]['url'], prop_filename, i)).start()  # 사진을 폴더에 저장
                             except:
-                                threading.Thread(target=get.imgpp, args=("https://picsum.photos/512", prop_filename, i)).start()
-                        prop_img = "../static/prop_img/"+str(prop_filename)+"_"+str(i)+".png"
-                        db.db_connector(f"REPLACE INTO prop(propid, productid, propname, price, orginalprice, img) VALUES('{n['properties']}', '{product_id}', '{prop_name}', '{n['price']}', '{n['orginal_price']}', '{prop_img}');")
-                    
-                    db.db_connector(f"REPLACE INTO taobao(productid, title, price, orginalprice) VALUES('{product_id}', '{title}', '{price}', '{orginal_price}');")
+                                threading.Thread(target=get.imgpp,
+                                                 args=("https://picsum.photos/512", prop_filename, i)).start()
+                        prop_img = "../static/prop_img/" + str(prop_filename) + "_" + str(i) + ".png"
+                        db.db_connector(
+                            f"REPLACE INTO prop(propid, productid, propname, price, orginalprice, img) VALUES('{n['properties']}', '{product_id}', '{prop_name}', '{n['price']}', '{n['orginal_price']}', '{prop_img}');")
+
+                    db.db_connector(
+                        f"REPLACE INTO taobao(productid, title, price, orginalprice) VALUES('{product_id}', '{title}', '{price}', '{orginal_price}');")
 
                     # 중복 검사 구문
                     try:
-                        productid = db.db_connector(f'''SELECT productid FROM userproduct WHERE userid="{session['user']}";''').split()
+                        productid = db.db_connector(
+                            f'''SELECT productid FROM userproduct WHERE userid="{session['user']}";''').split()
                         if not "{}".format(product_id) in productid:
-                            db.db_connector(f"INSERT INTO userproduct(userid, productid) VALUES('{session['user']}', '{product_id}');")
+                            db.db_connector(
+                                f"INSERT INTO userproduct(userid, productid) VALUES('{session['user']}', '{product_id}');")
                     except:
-                        db.db_connector(f"INSERT INTO userproduct(userid, productid) VALUES('{session['user']}', '{product_id}');")
+                        db.db_connector(
+                            f"INSERT INTO userproduct(userid, productid) VALUES('{session['user']}', '{product_id}');")
 
                     success_count += 1
                 except:
                     fail_count += 1
 
             flash('수집 완료')
-            return render_template(HTML_PATH_SCRAP, username=session['user'], userplan=session['userplan'], isresult=True, successcount=success_count, failcount=fail_count, count=success_count+fail_count)
+            return render_template(HTML_PATH_SCRAP, username=session['user'], userplan=session['userplan'],
+                                   isresult=True, successcount=success_count, failcount=fail_count,
+                                   count=success_count + fail_count)
         except:
             flash("수집 실패")
-            return render_template(HTML_PATH_SCRAP, username=session['user'], userplan=session['userplan'], isresult=True, failcount="오류", successcount="오류", count="오류")
+            return render_template(HTML_PATH_SCRAP, username=session['user'], userplan=session['userplan'],
+                                   isresult=True, failcount="오류", successcount="오류", count="오류")
+
 
 @app.route('/<path>/<imgname>', methods=['GET'])
 def image(path, imgname):
     return send_file("./static/{}/{}".format(path, imgname), mimetype='image/png')
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port="5555", threaded=True, debug=True)
